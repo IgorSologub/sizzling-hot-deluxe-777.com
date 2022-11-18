e37da47eabbec68325cecdd2de59f0993c2496ad
IgorSologub <sologub355@gmail.com>
2022-11-18 14:32:49 +0200

    Initial commit

---
+++ src/main/java/pages/onlineCasinos/OnlineCasinosPage.java
@@ -0,0 +1,18 @@
+package pages.onlineCasinos;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import pages.artikel.ArtikelPage;
+import pages.base.BasePage;
+
+public class OnlineCasinosPage extends BasePage {
+
+    public OnlineCasinosPage(WebDriver driver) {super(driver);}
+
+    public OnlineCasinosPage ubersichtRegButtonClick(int numButton) {
+        By ubersichtRegButton = By.xpath("(//a[contains(@class,'rev')])[" + numButton + "]");
+        driver.findElement(ubersichtRegButton).click();
+        return this;
+    }
+
+}
